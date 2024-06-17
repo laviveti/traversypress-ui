50df586b0364e2b76a71bbe40ee4be1b5bc44f80
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-06-17 13:29:32 -0300

    sidebar init

---
+++ components/Navbar.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/img/logo.png";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const Navbar = () => {
+  return (
+    <div className="bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between">
+      <Link href={"/"}>
+        <Image src={logo} alt="TravsersyPress" width={40} />
+      </Link>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger className=" focus:outline-none">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback className="text-slate-500">BT</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="mr-1">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href="/profile">Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href="/auth">Logout</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default Navbar;
