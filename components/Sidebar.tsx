770cfc03f86248e3c462bb7c92da0cc8618e855d
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-06-17 14:54:32 -0300

    feat: capítulo "Post Data Array & Types"

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,5 @@
 import {
   Command,
-  CommandDialog,
   CommandEmpty,
   CommandGroup,
   CommandInput,
@@ -21,20 +20,41 @@ import Link from "next/link";
 
 const Sidebar = () => {
   return (
-    <Command>
+    <Command className="bg-secondary rounded-none">
       <CommandInput placeholder="Type a command or search..." />
       <CommandList>
         <CommandEmpty>No results found.</CommandEmpty>
         <CommandGroup heading="Suggestions">
-          <CommandItem>Calendar</CommandItem>
-          <CommandItem>Search Emoji</CommandItem>
-          <CommandItem>Calculator</CommandItem>
+          <CommandItem>
+            <LayoutDashboard className=" size-4 mr-2" />
+            <Link href={"/"}>Dashboard</Link>
+          </CommandItem>
+          <CommandItem>
+            <Newspaper className=" size-4 mr-2" />
+            <Link href={"/posts"}>Posts</Link>
+          </CommandItem>
+          <CommandItem>
+            <Folders className=" size-4 mr-2" />
+            <Link href={"#"}>Categories</Link>
+          </CommandItem>
         </CommandGroup>
         <CommandSeparator />
         <CommandGroup heading="Settings">
-          <CommandItem>Profile</CommandItem>
-          <CommandItem>Billing</CommandItem>
-          <CommandItem>Settings</CommandItem>
+          <CommandItem>
+            <User className="mr-2 size-4" />
+            <span>Profile</span>
+            <CommandShortcut>Ctrl + P</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <CreditCard className="mr-2 size-4" />
+            <span>Billing</span>
+            <CommandShortcut>Ctrl + B</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <Settings className="mr-2 size-4" />
+            <span>Settings</span>
+            <CommandShortcut>Ctrl + S</CommandShortcut>
+          </CommandItem>
         </CommandGroup>
       </CommandList>
     </Command>
---
+++ components/dashboard/DashboardCard.tsx
@@ -0,0 +1,29 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { LucideIcon } from "lucide-react";
+import { FC } from "react";
+
+interface DashboardCardProps {
+  title: string;
+  count: number;
+  icon: React.ReactElement<LucideIcon>;
+}
+
+const DashboardCard: FC<DashboardCardProps> = ({ title, count, icon }) => {
+  return (
+    <Card>
+      <CardContent>
+        <h3 className=" text-3xl text-center mb-4 font-bold text-slate-500 dark:text-slate-200">
+          {title}
+        </h3>
+        <div className="flex gap-5 justify-center items-center">
+          {icon}
+          <h3 className="text-5xl font-semibold text-slate-500 dark:text-slate-200">
+            {count}
+          </h3>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default DashboardCard;
